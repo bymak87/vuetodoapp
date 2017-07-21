78e057d934a72188361aa1c958e526b364ca72cb
Amy Bakameyer <bymak87@gmail.com>
2017-07-20 23:02:25 -0500

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Completed </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['todos'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
