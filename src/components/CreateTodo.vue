304496c36d99c695a0a7d3223c7e2926c6317de2
Amy Bakameyer <bymak87@gmail.com>
2017-07-21 00:25:12 -0500

    added text to button

--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ui basic content center aligned segment">
     <button class='ui basic button icon' v-on:click="openForm" v-show="!isCreating">
-      <i class='plus icon'></i>
+      CREATE NEW<i class='plus icon'></i>
     </button>
     <div class='ui centered card' v-show="isCreating">
       <div class='content'>
